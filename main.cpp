834f0e5703bc72a6e57e601b08764e74d6a629bf
Matheus Ramos <47630965+MatheusMartinsDeRamos@users.noreply.github.com>
2022-09-27 23:12:56 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,9 +6,10 @@
 /* Esse código faz operações de matematica, tais como adição, subtração, multiplicação e divisão, potencialização e Raiz Quadrada */
 /* POTENCIALIZAçÂO:  https://pt.stackoverflow.com/questions/130147/como-usar-a-fun%C3%A7%C3%A3o-pow-potencia-em-c */
 /*RAIZ QUADRADA: http://linguagemc.com.br/a-biblioteca-math-h/ */
+/*Eu gostaria de melhorar o código verificando se o dado inputado não é uma string antes de seguir o código, assim não daria erro. Atualmente se o usuário coloca uma string está entrando em loop, mas ainda não sei como resolve isso. Se puder me enviar material sobre isso no email, eu agradeço: matheus.202213339@unilasalle.edu.br*/
 
 int main() {
-/*Variaveis INICIO*/
+/*As variaveis são declaradas aqui*/
   char aluno[100] = "Matheus Martins de Ramos";
   char curso[100]  = "Analise e Desenvolvimento de Sistemas";
   char universidade[100]  = "La Salle";
@@ -17,19 +18,19 @@ int main() {
   int opcaoMenu;
   bool mostrarMenu= true;
 
-/*Variaveis FIM*/
 
-/*Apresentação INICIO*/
+
+/*Aqui só temos a apresentação, pegando os dados das variaveis*/
   printf("_-_-_-_-_-_- \n");
   printf("Aluno: %s \n", aluno);
   printf("Curso: %s \n", curso);
   printf("Universidade: %s \n", universidade);
   printf("Matricula: %s \n", matricula);
   printf("_-_-_-_-_-_- \n");
-/*Apresentação FIM*/ 
+
   
  do {
-      /*Escolha da Operação INICIO*/
+      /*Aqui é onde o usuário deve escolher a opção desejada. Usei o do-while pois precisa ser executado PELO MENOS 1 vez*/
       printf("Digite o numero de qual operação deseja realizar \n");
       printf("  1.Potenciação \n");
       printf("  2.Soma \n");
@@ -39,20 +40,25 @@ int main() {
       printf("  6.Raiz Quadrada \n");
       printf("  0.SAIR \n");
       scanf("%d", &opcaoMenu);
-      /*Escolha da Operação FIM*/
+
+        /*A primeira validação é se a opção é zero, então já para a execução do programa aqui*/
       if (opcaoMenu == 0) {
       mostrarMenu = false;
       printf("Obrigado por usar nosso sistema! Até logo!");
       }
+       /*Verificando se é uma opção válida e se recebe dois números para o cálculo*/
       else if (opcaoMenu == 1 || opcaoMenu == 2 || opcaoMenu == 3 || opcaoMenu == 4 || opcaoMenu == 5){
-      /*mostrarMenu = true;*/
-      /*Escolha dos números INICIO*/
       printf("'=-=-=-=-=-=-=-=-=-=-=-=- \n");
       printf("Escolha o primeiro número: \n");
       scanf("%f", &numero1);
       printf("Escolha o segundo número: \n");
       scanf("%f", &numero2);
-      /*Escolha dos números FIM*/
+      /*É feita uma validação se a op~]ap é a divisão & se o segundo número preenchido foi zero. Se for informa mensagem de erro e volta para o menu inicial*/
+      if (opcaoMenu == 5 && numero2 == 0) {
+        printf("Parece que você está tentando dividir por zero, mas isso não é possivel, tente novamente. \n\n");
+      } else {
+      /*Fiz um switch case pois sei os valores possíveis, achei melhor que usar IFs*/
+      /*De acorodo com cada opção, faz o cálculo*/
       switch (opcaoMenu) {
       case 1:
       resultado = pow(numero1, numero2);       
@@ -67,11 +73,14 @@ int main() {
       case 5: resultado = numero1 / numero2;
       break;
       }
+        /*Apresentando o resultado*/
         printf("'=-=-=-=-=-=-=-=-=-=-=-=- \n");
         printf("O resultado é: %.2f \n\n", resultado);
         printf("'=-=-=-=-=-=-=-=-=-=-=-=- \n");
       }
-
+    }   
+        /*Caso a opção seja Raiz quadrada, deve solicitar apenas um número*/
+        /*Assim como naopção 1, usei uma biblioteca chamada math.h, pois nela já consigo usar a função pow, para potenciação e sqrt para raiz quadrada. Achar isso no google me fez ganahr um bom tempo*/
         else if (opcaoMenu == 6) {
       printf("Escolha um número: \n");
       scanf("%f", &numero1);
@@ -80,7 +89,7 @@ int main() {
       printf("O resultado é: %.2f \n\n", resultado);
       printf("'=-=-=-=-=-=-=-=-=-=-=-=- \n");
         }
-        
+        /*Aqui é quando o usuário preencher uma opção inválida*/
       else {
         printf("'=-=-=-=-=-=-=-=-=-=-=-=- \n");
         printf("'%d' Não é uma opção válida! \n", opcaoMenu);
